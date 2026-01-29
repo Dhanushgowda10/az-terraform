06110cf7874b3bd18fee5b5b53c633090119e867
Dhanush Gowda <145486119+Dhanushgowda10@users.noreply.github.com>
2026-01-29 10:38:42 +0530

    Update vnet.tf

--- az-module-terraform/modules/vnet.tf
+++ az-module-terraform/modules/vnet.tf
@@ -1,6 +1,7 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet1"
+  name                = var.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+
